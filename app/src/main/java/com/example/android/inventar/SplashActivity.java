38c52d7b6b913d0de31b951a57b7a85eb067b53a
Patrik <patriklarsson152@gmail.com>
2018-02-26 22:08:13 +0100

    First commit

---
+++ app/src/main/java/com/example/android/inventar/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.inventar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by patri on 2018-02-26.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
